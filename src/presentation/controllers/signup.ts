309f766770a359e0b640cbd3d3bcc5b8ca07fb27
alailsonko <aalailson3@gmail.com>
2020-07-12 14:28:02 -0300

    refactor: move code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,10 +17,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httRequest.body.password !== httRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httRequest.body
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const isValid = this.emailValidator.isValid(httRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
