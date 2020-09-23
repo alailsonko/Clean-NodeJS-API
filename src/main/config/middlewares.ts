6fbf9e909b5a6e8295a7334ba052d575ea7e1055
alailsonko <aalailson3@gmail.com>
2020-09-22 22:39:01 -0300

    feat: ensure requests return json as default content

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,11 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser'
+import { contentType } from '../middlewares/content-type'
 import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
-  const middlewares = [bodyParser, cors]
+  const middlewares = [bodyParser, cors, contentType]
 
-  for (const i of middlewares) {
-    app.use(i)
-  }
+  app.use(middlewares.map(item => item))
 }
